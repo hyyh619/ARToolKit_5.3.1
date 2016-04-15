03eb6223812340d5e997194b69e98e0bce005545
Huang Ying <hyyh619@hotmail.com>
2016-04-15 14:47:13 +0800

    ARToolKit: beautify code.
    By: ying.huang.

--- lib/SRC/AR/paramDisp.c
+++ lib/SRC/AR/paramDisp.c
@@ -35,82 +35,102 @@
  *
  */
 /*******************************************************
- *
- * Author: Takeshi Mita, Shinsaku Hiura, Hirokazu Kato
- *
- *         tmita@inolab.sys.es.osaka-u.ac.jp
- *         shinsaku@sys.es.osaka-u.ac.jp
- *         kato@sys.im.hiroshima-cu.ac.jp
- *
- * Revision: 4.1
- * Date: 01/12/07
- *
- *******************************************************/
+*
+* Author: Takeshi Mita, Shinsaku Hiura, Hirokazu Kato
+*
+*         tmita@inolab.sys.es.osaka-u.ac.jp
+*         shinsaku@sys.es.osaka-u.ac.jp
+*         kato@sys.im.hiroshima-cu.ac.jp
+*
+* Revision: 4.1
+* Date: 01/12/07
+*
+*******************************************************/
 
 #include <stdio.h>
 #include <stdarg.h>
 #include <AR/ar.h>
 
 
-int arParamDisp( const ARParam *param )
+int arParamDisp(const ARParam *param)
 {
-    int     i, j;
+    int i, j;
 
     ARLOG("--------------------------------------\n");
     ARLOG("SIZE = %d, %d\n", param->xsize, param->ysize);
-	if (param->dist_function_version == 4) {
-		ARLOG("Distortion factor: k1=%1.10f, k2=%1.10f, p1=%1.10f, p2=%1.10f\n", param->dist_factor[0],
-				param->dist_factor[1], param->dist_factor[2], param->dist_factor[3]);
-		ARLOG("                  fx=%f, fy=%f, x0=%f, y0=%f, s=%f\n", param->dist_factor[4],
-            param->dist_factor[5], param->dist_factor[6], param->dist_factor[7], param->dist_factor[8]);
-	} else if (param->dist_function_version == 3) {
-		ARLOG("Distortion factor = %f %f %f %f %f %f\n", param->dist_factor[0],
-			   param->dist_factor[1], param->dist_factor[2], param->dist_factor[3],
-			   param->dist_factor[4], param->dist_factor[5] );
-	} else if (param->dist_function_version == 2) {
-		ARLOG("Distortion factor = %f %f %f %f %f\n", param->dist_factor[0],
-			   param->dist_factor[1], param->dist_factor[2],
-			   param->dist_factor[3], param->dist_factor[4] );
-	} else if (param->dist_function_version == 1) {
-		ARLOG("Distortion factor = %f %f %f %f\n", param->dist_factor[0],
-			   param->dist_factor[1], param->dist_factor[2], param->dist_factor[3] );
-	} else {
-		ARLOG("Distortion factor = INVALID or UNKNOWN format\n");
-	}
-    for( j = 0; j < 3; j++ ) {
-        for( i = 0; i < 4; i++ ) ARLOG("%7.5f ", param->mat[j][i]);
+    if (param->dist_function_version == 4)
+    {
+        ARLOG("Distortion factor: k1=%1.10f, k2=%1.10f, p1=%1.10f, p2=%1.10f\n", param->dist_factor[0],
+              param->dist_factor[1], param->dist_factor[2], param->dist_factor[3]);
+        ARLOG("                  fx=%f, fy=%f, x0=%f, y0=%f, s=%f\n", param->dist_factor[4],
+              param->dist_factor[5], param->dist_factor[6], param->dist_factor[7], param->dist_factor[8]);
+    }
+    else if (param->dist_function_version == 3)
+    {
+        ARLOG("Distortion factor = %f %f %f %f %f %f\n", param->dist_factor[0],
+              param->dist_factor[1], param->dist_factor[2], param->dist_factor[3],
+              param->dist_factor[4], param->dist_factor[5]);
+    }
+    else if (param->dist_function_version == 2)
+    {
+        ARLOG("Distortion factor = %f %f %f %f %f\n", param->dist_factor[0],
+              param->dist_factor[1], param->dist_factor[2],
+              param->dist_factor[3], param->dist_factor[4]);
+    }
+    else if (param->dist_function_version == 1)
+    {
+        ARLOG("Distortion factor = %f %f %f %f\n", param->dist_factor[0],
+              param->dist_factor[1], param->dist_factor[2], param->dist_factor[3]);
+    }
+    else
+    {
+        ARLOG("Distortion factor = INVALID or UNKNOWN format\n");
+    }
+
+    for (j = 0; j < 3; j++)
+    {
+        for (i = 0; i < 4; i++)
+            ARLOG("%7.5f ", param->mat[j][i]);
+
         ARLOG("\n");
     }
+
     ARLOG("--------------------------------------\n");
 
     return 0;
 }
 
-int arParamDispExt( ARdouble trans[3][4] )
+int arParamDispExt(ARdouble trans[3][4])
 {
-    int     i, j;
-                                                                                
+    int i, j;
+
     ARLOG("--------------------------------------\n");
-    for( j = 0; j < 3; j++ ) {
-        for( i = 0; i < 4; i++ ) ARLOG("%7.5f ", trans[j][i]);
+
+    for (j = 0; j < 3; j++)
+    {
+        for (i = 0; i < 4; i++)
+            ARLOG("%7.5f ", trans[j][i]);
+
         ARLOG("\n");
     }
+
     ARLOG("--------------------------------------\n");
-                                                                                
+
     return 0;
 }
 
 int arParamDispOptical(const ARdouble fovy, const ARdouble aspect, const ARdouble m[16])
 {
-    int     i;
-	
+    int i;
+
     ARLOG("--------------------------------------\n");
-	ARLOG("Field-of-view vertical = %.1f, horizontal = %.1f degrees, aspect ratio = %.3f\n", fovy, fovy*aspect, aspect);
-	ARLOG("Transformation to camera, in eye coordinate system = \n");
-	for (i = 0; i < 4; i++) ARLOG("[% .3f % .3f % .3f] [% 6.1f]\n", m[i], m[i + 4], m[i + 8], m[i + 12]);
+    ARLOG("Field-of-view vertical = %.1f, horizontal = %.1f degrees, aspect ratio = %.3f\n", fovy, fovy * aspect, aspect);
+    ARLOG("Transformation to camera, in eye coordinate system = \n");
+
+    for (i = 0; i < 4; i++)
+        ARLOG("[% .3f % .3f % .3f] [% 6.1f]\n", m[i], m[i + 4], m[i + 8], m[i + 12]);
+
     ARLOG("--------------------------------------\n");
-	
+
     return 0;
 }
-
-
