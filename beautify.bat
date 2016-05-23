083672b62e7ec1389e1e648bfdecfe3ec974af52
Huang Ying <hyyh619@hotmail.com>
2016-05-23 15:26:27 +0800

    ARToolKit: update beautify command.
    By: ying.huang.

--- beautify.bat
+++ beautify.bat
@@ -1,1 +1,1 @@
-..\..\..\tools\uncrustify-0.62.0-g5987f22-win32\uncrustify.exe -c "..\..\tools\hy_c_c++.cfg" --no-backup -F cscope.files
+..\..\..\tools\uncrustify.exe -c "..\..\tools\hy_c_c++.cfg" --no-backup -F cscope.files
